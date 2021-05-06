4bd306efc054a50df03b7333c5b1e6b06ab534cc
frozen057 <hell-91@bk.ru>
2021-05-06 14:49:41 +0500

    1.3

--- test.sh
+++ test.sh
@@ -3,7 +3,8 @@
 DATE=`date "+%Y-%m-%d"`
 dest_dir="/tmp"
 # создание файлов
-echo "test" > $dest_dir/file_$DATE
+# echo "test" > $dest_dir/file_$DATE
+base64 /dev/urandom | head -c 10000000 > $dest_dir/file_$DATE
 # копирование файлов на удаленную машину
 rsync -avz $dest_dir/file_$DATE root@ip:tmp
 # Удаление ротация файлов на удаленной машине
