ae2e9752bd18367d144d08d06bb3758b2d2f4858
frozen057 <hell-91@bk.ru>
2021-05-11 16:46:08 +0500

    1.4

--- test.sh
+++ test.sh
@@ -6,6 +6,6 @@ dest_dir="/tmp"
 # echo "test" > $dest_dir/file_$DATE
 base64 /dev/urandom | head -c 10000000 > $dest_dir/file_$DATE
 # копирование файлов на удаленную машину
-rsync -avz $dest_dir/file_$DATE root@ip:tmp
+rsync -avz $dest_dir/file_$DATE root@10.172.0.3:/tmp
 # Удаление ротация файлов на удаленной машине
-ssh your_user@your_host "find $dest_dir/file* -mtime +7 -exec rm -rf {} \;"
+ssh root@10.172.0.3 "find $dest_dir/file* -mtime +7 -exec rm -rf {} \;"
